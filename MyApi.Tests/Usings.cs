3881c33dd548b84f000494b3b1461d8ff4d7cdd9
Daniel Opitz <d.opitz@outlook.com>
2022-12-19 19:43:37 +0100

    Init

--- MyApi.Tests/Usings.cs
+++ MyApi.Tests/Usings.cs
@@ -3,3 +3,5 @@ global using System.Net;
 global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
