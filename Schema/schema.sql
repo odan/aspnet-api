6fb3cdc77c4ec1a416601801a2298d5b59822cb6
Daniel Opitz <d.opitz@outlook.com>
2023-01-08 20:34:07 +0100

    Update

--- Schema/schema.sql
+++ Schema/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE `customers` (
   `username` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `schemaversions` (
   `schemaversionid` int(11) NOT NULL AUTO_INCREMENT,
